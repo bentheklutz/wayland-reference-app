c555583d8b0aac37e0e8def43f5d4d2ea3aca841
Benjamin Hetz <bwhdev@gmail.com>
2024-10-24 10:32:12 -0500

    Bugfixes and Cleanup
    - Rectangles now draw properly. Too many width and _width and whatnot
    - ^^ That's just software rendering so it's not going to be expanded
      anyway
    - Got rid of (most of?) the logs for events. They're mostly replaced
      with empty switches / handlers.
    - Moved the blasted Color struct def up out of all of the wayland
      handler functions.

--- src/main.zig
+++ src/main.zig
@@ -6,11 +6,13 @@ const wayland = @import("wayland");
 const wl = wayland.client.wl;
 const xdg = wayland.client.xdg;
 const xkb = @import("xkbcommon");
+const input_event_codes = @cImport(@cInclude("linux/input-event-codes.h"));
 
 // const pipewire = @import("pipewire-0.3_2.zig");
 
 const Context = struct {
     running: bool,
+    last_frame_time: u32,
     shm: ?*wl.Shm,
     compositor: ?*wl.Compositor,
     wm_base: ?*xdg.WmBase,
@@ -31,61 +33,74 @@ const SurfaceContext = struct {
     xdg_toplevel: ?*xdg.Toplevel,
 };
 
-const width = 640;
-const height = 640;
+const width = 1280;
+const height = 720;
 const bytes_per_pixel = 4;
 const stride = width * bytes_per_pixel;
 
+const grid_rows = 10;
+const grid_cols = 10;
+
+/// Coordinates and velocities in grid space
 var player = struct {
-    x: usize,
-    y: usize,
+    x: i32,
+    y: i32,
+    dx: i32,
+    dy: i32,
 }{
     .x = width / 2,
     .y = height / 2,
+    .dx = 0.0,
+    .dy = 0.0,
 };
 
-const color_background = Color(u8){
-    .red = 0x00,
-    .green = 0x48,
-    .blue = 0x64,
-    .alpha = 0xFF,
-};
-const color_player =
-    Color(u8){
-    .red = 0xDD,
-    .green = 33,
-    .blue = 0x64,
-    .alpha = 0xFF,
+const player_size = 30;
+
+const Color = struct {
+    red: u8,
+    green: u8,
+    blue: u8,
+    alpha: u8,
 };
 
-fn render(buf: []u8, bg: Color(u8), p: Color(u8)) void {
-    for (0..height) |y| {
-        for (0..width) |x| {
-            const idx = bytes_per_pixel * (y * height + x);
-            buf[idx + 0] = bg.blue;
-            buf[idx + 1] = bg.green;
-            buf[idx + 2] = bg.red;
-            buf[idx + 3] = bg.alpha;
-        }
-    }
+const color_background = Color{ .red = 0x00, .green = 0x48, .blue = 0x64, .alpha = 0xFF };
+const color_player = Color{ .red = 0xDD, .green = 33, .blue = 0x64, .alpha = 0xFF };
 
-    const player_size = 30;
-
-    for (0..player_size) |y| {
-        for (0..player_size) |x| {
-            const pos_x = player.x + x - player_size / 2;
-            const pos_y = player.y + y - player_size / 2;
-            if (pos_x < 0 or pos_x > width or pos_y < 0 or pos_y > height) {
+fn drawRectangle(buf: []u8, color: Color, _x: i32, _y: i32, _width: u32, _height: u32) void {
+    const xu: usize = @intCast(_x);
+    const yu: usize = @intCast(_y);
+    for (0.._height) |y| {
+        for (0.._width) |x| {
+            const pos_x = xu + x - _width / 2;
+            const pos_y = yu + y - _height / 2;
+            if (pos_x > width or pos_y > height) {
                 continue;
             }
 
-            const idx = bytes_per_pixel * (pos_y * height + pos_x);
-            buf[idx + 0] = p.blue;
-            buf[idx + 1] = p.green;
-            buf[idx + 2] = p.red;
-            buf[idx + 3] = p.alpha;
+            const idx = bytes_per_pixel * (pos_y * width + pos_x);
+            buf[idx + 0] = color.blue;
+            buf[idx + 1] = color.green;
+            buf[idx + 2] = color.red;
+            buf[idx + 3] = color.alpha;
         }
     }
+}
+
+fn render(buf: []u8, bg: Color, p: Color) void {
+    // Background
+    drawRectangle(buf, bg, width / 2, height / 2, width, height);
+
+    // Grid outline for whatever reason
+    for (1..grid_cols) |i| {
+        const x: i32 = @intCast(width / grid_cols * i);
+        drawRectangle(buf, .{ .red = 0x88, .green = 0x88, .blue = 0xDD, .alpha = 0xFF }, x, height / 2, 1, height);
+    }
+    for (1..grid_rows) |j| {
+        const y: i32 = @intCast(height / grid_rows * j);
+        drawRectangle(buf, .{ .red = 0x88, .green = 0x88, .blue = 0xDD, .alpha = 0xFF }, width / 2, y, width, 1);
+    }
+    // User-controlled "player" (WASD)
+    drawRectangle(buf, p, player.x, player.y, player_size, player_size);
 }
 
 pub fn main() !void {
@@ -99,6 +114,7 @@ pub fn main() !void {
 
     var context = Context{
         .running = true,
+        .last_frame_time = 0,
         .shm = null,
         .compositor = null,
         .wm_base = null,
@@ -153,21 +169,7 @@ pub fn main() !void {
         // 121856 total bytes
         context.surface_ctx.display_data = data;
 
-        render(
-            data,
-            Color(u8){
-                .red = 0x00,
-                .green = 0x48,
-                .blue = 0x64,
-                .alpha = 0xFF,
-            },
-            Color(u8){
-                .red = 0xDD,
-                .green = 33,
-                .blue = 0x64,
-                .alpha = 0xFF,
-            },
-        );
+        render(data, color_background, color_player);
 
         const pool = try shm.createPool(fd, size);
         defer pool.destroy();
@@ -188,6 +190,9 @@ pub fn main() !void {
     context.surface_ctx.xdg_toplevel = xdg_toplevel;
     defer xdg_toplevel.destroy();
 
+    const frame_callback = try surface.frame();
+    frame_callback.setListener(*Context, surfaceFrameListener, &context);
+
     xdg_surface.setListener(*SurfaceContext, xdgSurfaceListener, &context.surface_ctx);
     xdg_toplevel.setListener(*Context, xdgToplevelListener, &context);
 
@@ -203,7 +208,45 @@ pub fn main() !void {
     while (context.running) {
         if (display.dispatch() != .SUCCESS) return error.DispatchFailed;
 
-        surface.damage(0, 0, width, height);
+        player.x += player.dx;
+        player.y += player.dy;
+        if (player.x < player_size / 2) {
+            player.x = player_size / 2;
+        }
+        if (player.y < player_size / 2) {
+            player.y = player_size / 2;
+        }
+        if (player.x > width - player_size / 2) {
+            player.x = width - player_size / 2;
+        }
+        if (player.y > height - player_size / 2) {
+            player.y = height - player_size / 2;
+        }
+        // render(context.surface_ctx.display_data.?, color_background, color_player);
+        // surface.attach(buffer, 0, 0);
+        // surface.damageBuffer(0, 0, width, height);
+        // surface.commit();
+        // _ = display.flush();
+    }
+}
+
+fn surfaceFrameListener(cb: *wl.Callback, event: wl.Callback.Event, context: *Context) void {
+    switch (event) {
+        .done => {
+            cb.destroy();
+            if (context.surface_ctx.surface) |surface| {
+                const display_data = context.surface_ctx.display_data.?;
+                const new_callback = surface.frame() catch |e| {
+                    std.log.err("Failed to create frame callback: {s}", .{@errorName(e)});
+                    return;
+                };
+                new_callback.setListener(*Context, surfaceFrameListener, context);
+                render(display_data, color_background, color_player);
+                surface.attach(context.surface_ctx.buffer, 0, 0);
+                surface.damageBuffer(0, 0, width, height);
+                context.surface_ctx.surface.?.commit();
+            }
+        },
     }
 }
 
@@ -211,7 +254,6 @@ fn bufferListener(buffer: *wl.Buffer, event: wl.Buffer.Event, context: *SurfaceC
     _ = context;
     switch (event) {
         .release => {
-            std.log.debug("Gotta release the buffer now", .{});
             buffer.destroy();
         },
     }
@@ -229,23 +271,23 @@ fn surfaceListener(surface: *wl.Surface, event: wl.Surface.Event, context: *Surf
 fn xdgWmBaseListener(xdg_wm_base: *xdg.WmBase, event: xdg.WmBase.Event, context: *Context) void {
     _ = context;
     switch (event) {
-        .ping => |p| {
-            xdg_wm_base.pong(p.serial);
+        .ping => |evt| {
+            xdg_wm_base.pong(evt.serial);
         },
     }
 }
 
 fn registryListener(registry: *wl.Registry, event: wl.Registry.Event, context: *Context) void {
     switch (event) {
-        .global => |global| {
-            if (std.mem.orderZ(u8, global.interface, wl.Compositor.getInterface().name) == .eq) {
-                context.compositor = registry.bind(global.name, wl.Compositor, 1) catch return;
-            } else if (std.mem.orderZ(u8, global.interface, wl.Shm.getInterface().name) == .eq) {
-                context.shm = registry.bind(global.name, wl.Shm, 1) catch return;
-            } else if (std.mem.orderZ(u8, global.interface, xdg.WmBase.getInterface().name) == .eq) {
-                context.wm_base = registry.bind(global.name, xdg.WmBase, 1) catch return;
-            } else if (std.mem.orderZ(u8, global.interface, wl.Seat.getInterface().name) == .eq) {
-                context.seat = registry.bind(global.name, wl.Seat, 1) catch return;
+        .global => |evt| {
+            if (std.mem.orderZ(u8, evt.interface, wl.Compositor.getInterface().name) == .eq) {
+                context.compositor = registry.bind(evt.name, wl.Compositor, 6) catch return;
+            } else if (std.mem.orderZ(u8, evt.interface, wl.Shm.getInterface().name) == .eq) {
+                context.shm = registry.bind(evt.name, wl.Shm, 1) catch return;
+            } else if (std.mem.orderZ(u8, evt.interface, xdg.WmBase.getInterface().name) == .eq) {
+                context.wm_base = registry.bind(evt.name, xdg.WmBase, 1) catch return;
+            } else if (std.mem.orderZ(u8, evt.interface, wl.Seat.getInterface().name) == .eq) {
+                context.seat = registry.bind(evt.name, wl.Seat, 1) catch return;
             }
         },
         .global_remove => {},
@@ -255,14 +297,14 @@ fn registryListener(registry: *wl.Registry, event: wl.Registry.Event, context: *
 fn keyboardListener(keyboard: *wl.Keyboard, event: wl.Keyboard.Event, context: *Context) void {
     _ = keyboard;
     switch (event) {
-        .keymap => |km| {
-            defer std.posix.close(km.fd);
+        .keymap => |evt| {
+            defer std.posix.close(evt.fd);
             // TODO(Benjamin): This needs to map the fd from the event and read the keymap from it
-            if (km.format != .xkb_v1) {
-                std.log.err("Invalid keymap format: {s}\n", .{@tagName(km.format)});
+            if (evt.format != .xkb_v1) {
+                std.log.err("Invalid keymap format: {s}\n", .{@tagName(evt.format)});
                 return;
             }
-            const keymap = std.posix.mmap(null, km.size, std.posix.PROT.READ, .{ .TYPE = .PRIVATE }, km.fd, 0) catch |err| {
+            const keymap = std.posix.mmap(null, evt.size, std.posix.PROT.READ, .{ .TYPE = .PRIVATE }, evt.fd, 0) catch |err| {
                 std.log.err("Failed to mmap keymap fd: {s}", .{@errorName(err)});
                 return;
             };
@@ -283,10 +325,10 @@ fn keyboardListener(keyboard: *wl.Keyboard, event: wl.Keyboard.Event, context: *
         },
         .enter => {},
         .leave => {},
-        .key => |k| {
+        .key => |evt| {
             const xs = context.xkb_state orelse return;
 
-            const keycode = k.key + 8;
+            const keycode = evt.key + 8;
             const keysym = xs.keyGetOneSym(keycode);
             if (keysym == .NoSymbol) return;
 
@@ -295,22 +337,38 @@ fn keyboardListener(keyboard: *wl.Keyboard, event: wl.Keyboard.Event, context: *
                 xkb.Keysym.KP_Enter,
                 => {},
                 xkb.Keysym.W, xkb.Keysym.w => {
-                    player.y += 1;
+                    if (evt.state == .pressed) {
+                        player.dy += -5;
+                    } else {
+                        player.dy -= -5;
+                    }
                 },
                 xkb.Keysym.A,
                 xkb.Keysym.a,
                 => {
-                    player.x -= 1;
+                    if (evt.state == .pressed) {
+                        player.dx += -5;
+                    } else {
+                        player.dx -= -5;
+                    }
                 },
                 xkb.Keysym.S,
                 xkb.Keysym.s,
                 => {
-                    player.y -= 1;
+                    if (evt.state == .pressed) {
+                        player.dy += 5;
+                    } else {
+                        player.dy -= 5;
+                    }
                 },
                 xkb.Keysym.D,
                 xkb.Keysym.d,
                 => {
-                    player.x += 1;
+                    if (evt.state == .pressed) {
+                        player.dx += 5;
+                    } else {
+                        player.dx -= 5;
+                    }
                 },
                 xkb.Keysym.Escape => {},
                 xkb.Keysym.u => {},
@@ -321,15 +379,15 @@ fn keyboardListener(keyboard: *wl.Keyboard, event: wl.Keyboard.Event, context: *
                 else => {},
             }
         },
-        .modifiers => |m| {
+        .modifiers => |evt| {
             if (context.xkb_state) |xs| {
-                _ = xs.updateMask(m.mods_depressed, m.mods_latched, m.mods_locked, 0, 0, m.group);
+                _ = xs.updateMask(evt.mods_depressed, evt.mods_latched, evt.mods_locked, 0, 0, evt.group);
             }
         },
-        .repeat_info => |ri| {
-            context.*.key_repeat_delay = ri.delay;
-            context.*.key_repeat_rate = ri.rate;
-            std.debug.print("Keys will repeat after {d}ms at a rate of {d}/s", .{ ri.delay, ri.rate });
+        .repeat_info => |evt| {
+            context.*.key_repeat_delay = evt.delay;
+            context.*.key_repeat_rate = evt.rate;
+            std.debug.print("Keys will repeat after {d}ms at a rate of {d}/s", .{ evt.delay, evt.rate });
         },
     }
 }
@@ -338,78 +396,84 @@ fn pointerListener(pointer: *wl.Pointer, event: wl.Pointer.Event, context: *Cont
     _ = pointer;
     _ = context;
     switch (event) {
-        .enter => {
-            std.debug.print("The pointer has entered the area\n", .{});
+        .enter => {},
+        .leave => {},
+        .button => |evt| {
+            switch (evt.button) {
+                input_event_codes.BTN_LEFT => {},
+                input_event_codes.BTN_RIGHT => {},
+                input_event_codes.BTN_MIDDLE => {},
+                else => {},
+            }
         },
-        .leave => {
-            std.debug.print("The pointer has left the area\n", .{});
+        .axis => |evt| {
+            switch (evt.axis) {
+                .horizontal_scroll => {},
+                .vertical_scroll => {},
+                else => {},
+            }
         },
-        .button => |btn| {
-            std.debug.print("The pointer has pressed a button {}\n", .{btn.button});
+        .motion => |evt| {
+            _ = evt.surface_x;
+            _ = evt.surface_y;
+            _ = evt.time;
         },
-        .axis => |a| {
-            std.debug.print("The pointer has scrolled {}\n", .{a.axis});
+        .axis_source => |evt| {
+            switch (evt.axis_source) {
+                .wheel => {},
+                .finger => {},
+                .continuous => {},
+                else => {},
+            }
         },
-        .motion => |m| {
-            std.debug.print("The pointer has moved: ({}, {})\n", .{ m.surface_x, m.surface_y });
+        .axis_discrete => |evt| {
+            switch (evt.axis) {
+                .horizontal_scroll => {},
+                .vertical_scroll => {},
+                else => {},
+            }
         },
-        .axis_source => |as| {
-            std.debug.print("Axis source event {s}", .{@tagName(as.axis_source)});
-        },
-        .axis_discrete => |ad| {
-            std.debug.print("Axis discrete event: {s} - {d}", .{ @tagName(ad.axis), ad.discrete });
-        },
-        .axis_stop => |as| {
-            std.debug.print("Axis stop: {s}, {}", .{ @tagName(as.axis), as.time });
+        .axis_stop => |evt| {
+            switch (evt.axis) {
+                .horizontal_scroll => {},
+                .vertical_scroll => {},
+                else => {},
+            }
         },
         .frame => {
-            std.debug.print("Pointer frame marker", .{});
+            // TODO: Between frame events, motion, axis, etc events are meant to be
+            // accumulated into one atomic thing. You might have press down and move
+            // to a new position happen at the same time for example.
         },
-    }
-}
-
-fn Color(comptime T: type) type {
-    switch (T) {
-        f32, f64, u32, i32, u8, i8 => {
-            return struct {
-                red: T,
-                green: T,
-                blue: T,
-                alpha: T,
-            };
-        },
-        else => @compileError("Color only supports component types of 32/64 bit float and 8/32 bit int"),
     }
 }
 
 fn seatListener(seat: *wl.Seat, event: wl.Seat.Event, context: *Context) void {
     switch (event) {
-        .capabilities => |cap| {
-            if (cap.capabilities.pointer) {
+        .capabilities => |evt| {
+            if (evt.capabilities.pointer) {
                 context.pointer = seat.getPointer() catch return;
                 const pointer = context.pointer.?;
                 pointer.setListener(*Context, pointerListener, context);
             }
-            if (cap.capabilities.keyboard) {
+            if (evt.capabilities.keyboard) {
                 context.keyboard = seat.getKeyboard() catch return;
                 const keyboard = context.keyboard.?;
                 keyboard.setListener(*Context, keyboardListener, context);
             }
         },
-        .name => |n| {
-            std.log.debug("Hi, my name is {s}", .{n.name});
-        },
+        .name => {},
     }
 }
 
 fn xdgSurfaceListener(xdg_surface: *xdg.Surface, event: xdg.Surface.Event, context: *SurfaceContext) void {
     switch (event) {
-        .configure => |configure| {
-            std.log.debug("CONGIURE", .{});
-            xdg_surface.ackConfigure(configure.serial);
+        .configure => |evt| {
+            xdg_surface.ackConfigure(evt.serial);
             if (context.surface) |surface| {
                 // Shouldn't get here without a display buffer I believe
                 render(context.display_data.?, color_background, color_player);
+                surface.attach(context.buffer, 0, 0);
                 surface.commit();
             }
         },
